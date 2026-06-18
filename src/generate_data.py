92390a061774d135c25c1612f6fff923c099c3e5
ManojMeen0001 <manojofficialmail689@gmail.com>
2026-06-18 15:31:56 +0530

    feat:generate synthetic data for e-commerce platform

--- src/generate_data.py
+++ src/generate_data.py
@@ -1,41 +1,26 @@
-import random 
-import pandas as pd 
-from datetime import datetime,timedelta
-from faker import Faker
+import pandas as pd
+import random
 from db_connect import get_db_connection
-
+from datetime import datetime , timedelta
+from faker import Faker
+# Initialise faker 
 fake=Faker()
 
-def generate_customers(num_customers=100):   #  function made for customer data generation
-    
+
+def generate_ecom_data(num_customers=100,num_transactions=300):
+    # Generates structured relational data matrices matching your SQL Schema perfectly.
     customers=[]
-
-    cities=['Bengaluru','Pune','Hyderabad','Delhi','Mumbai','Indore','Chennai','Kolkata','Bhubaneshwar','Ahemadabad','Dehradun','Nagpur','Thiruvanantpuram']
-
-    for _ in range(num_customers):
+    cities = ['Bengaluru','Pune','Hyderabad','Delhi','Mumbai','Indore','Chennai','Kolkata','Bhubaneshwar','Ahemadabad','Dehradun','Nagpur','Thiruvanantpuram']
+    for i in range(1,num_customers+1):
         name=fake.name()
-        email=f"{name.lower().replace(' ','')}@{fake.free_email_domain()}"
+        # Clean processing: lowercase names and strip whitespaces for standard email sanitization# Clean processing: lowercase names and strip whitespaces for standard email sanitization
+        email=f'{name.lower().replace(' ','')}@{fake.free_email_domain()}'
         signup_date=fake.date_between(start_date='-1y',end_date='today')
-        city=random.choice(cities)
-
-        customers.append({'customer_name':name,
-                          'email':email,
-                          'signup_date':signup_date,
-                          'city':city
-                          })
-    return pd.DataFrame(customers)
+    customers.append({'customer_id':i,
+                      'customer_name':name,
+                      'email':email,
+                      'signup_date':signup_date,
+                      'city':random.choice(cities)})
+    df_customers=pd.DataFrame(customers)
 
 
-
-
-#------------------------------------------------------
-
-
-
-if __name__ == "__main__" :
-    engine=get_db_connection()
-    if engine:
-        print('🚀 Starting Data Generation Pipeline .... ')
-        df_customers=pd.DataFrame(generate_customers(100))
-        print(f"📊 Previewing {len(df_customers)} generated customer records:")
-        print(df_customers.head())
